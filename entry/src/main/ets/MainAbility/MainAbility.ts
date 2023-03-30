53f9cc5af7cfe9e72492b06a81b7bbe7322f6f02
jiangbinghan <jiangbinghan@huawei.com>
2023-03-30 19:47:10 +0800

    update
    Signed-off-by: jiangbinghan <jiangbinghan@huawei.com>

--- entry/src/main/ets/MainAbility/MainAbility.ts
+++ entry/src/main/ets/MainAbility/MainAbility.ts
@@ -3,20 +3,22 @@ import Window from '@ohos.window'
 
 import HiLog from "../utils/HiLog";
 import MmsPreferences from "../utils/MmsPreferences";
-import WantUtil from "../utils/WantUtil";
 import MmsDatabaseHelper from "../utils/MmsDatabaseHelper";
+import WorkFactory, { WorkerType } from "../workers/WorkFactory";
+import simCardService from "../service/SimCardService";
 
 const TAG = "app";
 
 export default class MainAbility extends Ability {
     onCreate(want, launchParam) {
-        HiLog.i(TAG, "Ability onCreate com.ohos.mms version: 1.0.0.34");
+        HiLog.i(TAG, "Ability onCreate com.ohos.mms version: 1.0.0.38");
         globalThis.mmsContext = this.context;
         globalThis.abilityWant = want;
         globalThis.MmsDatabaseHelper = new MmsDatabaseHelper();
         globalThis.MmsDatabaseHelper.createTable();
         globalThis.needToUpdate = true;
         MmsPreferences.getInstance().initPreferences();
+        globalThis.DataWorker = WorkFactory.getWorker(WorkerType.DataWorker);
     }
 
     onNewWant(want, launchParam) {
@@ -43,10 +45,17 @@ export default class MainAbility extends Ability {
     onForeground() {
         // Ability has brought to foreground
         HiLog.i(TAG, 'Ability onForeground');
+        simCardService.init();
     }
 
     onBackground() {
         // Ability has back to background
         HiLog.i(TAG, 'Ability onBackground');
+        simCardService.deInit();
+    }
+
+    onDestroy() {
+        HiLog.i(TAG, 'Ability onDestroy');
+        globalThis.DataWorker.close();
     }
 }
